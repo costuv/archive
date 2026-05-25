6c3a916ddbf9237a8a63a107b10d0b410c4084b8
kaustuvdhungel <study.kaustuvdhungel@gmail.com>
2026-05-25 21:53:25 +0545

    django deployment testing

--- archive_core/settings.py
+++ archive_core/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY', default='django-insecure-4(*3+6oz1_ogbyd)e084m
 DEBUG = config('DEBUG', default=True, cast=bool)
 
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='127.0.0.1,localhost', cast=Csv())
+ALLOWED_HOSTS = ['archive-2fhs.onrender.com']
 
 
 # Application definition
