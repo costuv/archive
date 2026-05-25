85d798fbca5cab213284c3c7bb1eb8d3ed4bb99a
kaustuvdhungel <study.kaustuvdhungel@gmail.com>
2026-05-26 00:35:40 +0545

    Add collectstatic to release

--- create_admin.py
+++ create_admin.py
@@ -1,20 +1,21 @@
-# create_admin.py
 import os
 import django
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "archive_core.settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'archive_core.settings')
 django.setup()
 
-from django.contrib.auth import get_user_model
+# Run migrations first
+from django.core.management import call_command
+call_command('migrate', '--run-syncdb')
 
-User = get_user_model()
+from django.contrib.auth.models import User
 
-username = "kaustuv"
-email = "kaustuvdhungel@gmail.com"
-password = "Kaustuv@123"
+username = os.environ.get('DJANGO_SUPERUSER_USERNAME', 'admin')
+email = os.environ.get('DJANGO_SUPERUSER_EMAIL', 'admin@example.com')
+password = os.environ.get('DJANGO_SUPERUSER_PASSWORD', 'admin')
 
 if not User.objects.filter(username=username).exists():
     User.objects.create_superuser(username=username, email=email, password=password)
-    print("Superuser created")
+    print(f'Superuser {username} created.')
 else:
-    print("Superuser already exists")
+    print(f'Superuser {username} already exists.')
